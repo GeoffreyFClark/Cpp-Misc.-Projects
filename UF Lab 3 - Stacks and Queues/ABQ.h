719b878acfed462243960908e5b22f4eae967582
Geoffrey F. Clark <97141856+GeoffreyFClark@users.noreply.github.com>
2023-06-13 19:50:55 -0600

    Added ABQ

--- UF Lab 3 - Stacks and Queues/ABQ.h
+++ UF Lab 3 - Stacks and Queues/ABQ.h
@@ -1,1 +1,147 @@
 #pragma once
+#include <stdexcept>
+
+template<typename T>
+class ABQ {
+private:
+    T* _dataQueue;
+    unsigned int _currentSize;
+    unsigned int _capacity;
+    float _SCALE_FACTOR;
+
+public:
+    ABQ();
+    ABQ(int capacity);
+    ABQ(const ABQ& q);
+    ABQ& operator=(const ABQ& q);
+    ~ABQ();
+
+    void enqueue(T data);
+    T dequeue();
+    T peek();
+    unsigned int getSize();
+    unsigned int getMaxCapacity();
+    T* getData();
+};
+
+// =====================
+
+template<typename T>
+ABQ<T>::ABQ() {
+    _capacity = 1;
+    _currentSize = 0;
+    _dataQueue = new T[_capacity];
+    _SCALE_FACTOR = 2.0f;
+}
+
+template<typename T>
+ABQ<T>::ABQ(int capacity) {
+    _capacity = capacity;
+    _currentSize = 0;
+    _dataQueue = new T[_capacity];
+    _SCALE_FACTOR = 2.0f;
+}
+
+template<typename T>
+ABQ<T>::ABQ(const ABQ& q) {
+    _capacity = q._capacity;
+    _currentSize = q._currentSize;
+    _dataQueue = new T[_capacity];
+    _SCALE_FACTOR = q._SCALE_FACTOR;
+
+    for (unsigned int i = 0; i < _currentSize; i++) {
+        _dataQueue[i] = q._dataQueue[i];
+    }
+}
+
+template<typename T>
+ABQ<T>& ABQ<T>::operator=(const ABQ& q) {
+    if (this == &q) {
+        return *this;
+    }
+
+    _capacity = q._capacity;
+    _currentSize = q._currentSize;
+    _SCALE_FACTOR = q._SCALE_FACTOR;
+
+    delete[] _dataQueue;
+    _dataQueue = new T[_capacity];
+
+    for (unsigned int i = 0; i < _currentSize; i++) {
+        _dataQueue[i] = q._dataQueue[i];
+    }
+
+    return *this;
+}
+
+template<typename T>
+ABQ<T>::~ABQ() {
+    delete[] _dataQueue;
+}
+
+template<typename T>
+void ABQ<T>::enqueue(T data) {
+    if (_currentSize == _capacity) {
+        _capacity *= _SCALE_FACTOR;
+        T* temp_dataQueue = new T[_capacity];
+        for (unsigned int i = 0; i < _currentSize; i++) {
+            temp_dataQueue[i] = _dataQueue[i];
+        }
+        delete[] _dataQueue;
+        _dataQueue = temp_dataQueue;
+    }
+
+    _dataQueue[_currentSize] = data;
+    _currentSize++;
+}
+
+template<typename T>
+T ABQ<T>::dequeue() {
+    if (_currentSize == 0) {
+        throw std::runtime_error("Queue is empty");
+    }
+
+    T dequeuedElement = _dataQueue[0];
+
+    for (unsigned int i = 1; i < _currentSize; i++) {
+        _dataQueue[i - 1] = _dataQueue[i];
+    }
+
+    _currentSize--;
+
+    if (static_cast<float>(_currentSize) / _capacity < 1 / _SCALE_FACTOR) {
+        _capacity /= static_cast<int>(_SCALE_FACTOR);
+        T* temp_dataQueue = new T[_capacity];
+        for (unsigned int i = 0; i < _currentSize; i++) {
+            temp_dataQueue[i] = _dataQueue[i];
+        }
+        delete[] _dataQueue;
+        _dataQueue = temp_dataQueue;
+    }
+
+    return dequeuedElement;
+}
+
+template<typename T>
+T ABQ<T>::peek() {
+    if (_currentSize == 0) {
+        throw std::runtime_error("Queue is empty");
+    }
+
+    return _dataQueue[0];
+}
+
+template<typename T>
+unsigned int ABQ<T>::getSize() {
+    return _currentSize;
+}
+
+template<typename T>
+unsigned int ABQ<T>::getMaxCapacity() {
+    return _capacity;
+}
+
+template<typename T>
+T* ABQ<T>::getData() {
+    return _dataQueue;
+}
--- UF Lab 3 - Stacks and Queues/main.cpp
+++ UF Lab 3 - Stacks and Queues/main.cpp
@@ -1,3 +1,4 @@
+/* PART 1: Array Based Stack
 #include <iostream>
 #include "ABS.h"
 #include "leaker.h"
@@ -46,6 +47,62 @@ int main()
 		cout << "\nPopped " << floatABS.pop() << endl;
 		cout << "New Size: " << floatABS.getSize() << endl;
 		cout << "New Max Capacity: " << floatABS.getMaxCapacity() << endl;
+	}
+
+	return 0;
+}
+*/
+
+// PART 2: Array Based Queue
+
+#include <iostream>
+#include "ABQ.h"
+#include "leaker.h"
+using namespace std;
+
+int main()
+{
+	cout << "Making integer ABQ...\n";
+	ABQ<int> intABQ;
+	cout << "Size: " << intABQ.getSize() << endl;
+	cout << "Max Capacity: " << intABQ.getMaxCapacity() << endl;
+	cout << "\nEnqueueing items...\n";
+	for (int i = 1; i < 10; i++)
+	{
+		intABQ.enqueue(i);
+		cout << "\nEnqueued " << i << endl;
+		cout << "New Size: " << intABQ.getSize() << endl;
+		cout << "New Max Capacity: " << intABQ.getMaxCapacity() << endl;
+	}
+
+	cout << "\nDequeueing items...\n";
+	for (int i = 1; i < 10; i++)
+	{
+		cout << "\nDequeued " << intABQ.dequeue() << endl;
+		cout << "New Size: " << intABQ.getSize() << endl;
+		cout << "New Max Capacity: " << intABQ.getMaxCapacity() << endl;
+	}
+
+
+	cout << "\nMaking float ABQ...\n";
+	ABQ<float> floatABQ(10);
+	cout << "Size: " << floatABQ.getSize() << endl;
+	cout << "Max Capacity: " << floatABQ.getMaxCapacity() << endl;
+	cout << "\nEnqueueing items...\n";
+	for (float i = 1; i < 5; i += 0.5f)
+	{
+		floatABQ.enqueue(i);
+		cout << "\nEnqueued " << i << endl;
+		cout << "New Size: " << floatABQ.getSize() << endl;
+		cout << "New Max Capacity: " << floatABQ.getMaxCapacity() << endl;
+	}
+
+	cout << "\nDequeueing items...\n";
+	for (float i = 1; i < 5; i += 0.5f)
+	{
+		cout << "\nDequeued " << floatABQ.dequeue() << endl;
+		cout << "New Size: " << floatABQ.getSize() << endl;
+		cout << "New Max Capacity: " << floatABQ.getMaxCapacity() << endl;
 	}
 
 	return 0;
